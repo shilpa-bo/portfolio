6fd18f3581b848fa611362b4e090a9055db3dd05
Shilpa Bojjireddy <shilpabojjireddy_work@Shilpas-MacBook-Pro.local>
2025-08-02 00:44:01 -0500

    add base path

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,29 @@
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   output: 'export',
+//   distDir: 'dist',
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+ 
+// module.exports = nextConfig
+
 /**
  * @type {import('next').NextConfig}
  */
+
+
 const nextConfig = {
+  basePath: '/portfolio',
   output: 'export',
+// Change the output directory `out` -> `dist`
   distDir: 'dist',
-  images: {
+  images:{
     unoptimized: true,
-  },
+  }
 }
  
 module.exports = nextConfig
